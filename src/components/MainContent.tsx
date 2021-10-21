a8d75ddc1f9eaead3b0d4aa517705b5d0ee3c2c4
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-10-21 14:38:09 +0100

    Changed location to Bangkok, Thailand

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -2,8 +2,8 @@ import { capitalise } from "../utils/capitalise";
 
 function MainContent(): JSX.Element {
   const location = {
-    city: "Kuala Lumpur",
-    country: "Malaysia",
+    city: "Bangkok",
+    country: "Thailand",
   };
 
   const numOne = 3;
