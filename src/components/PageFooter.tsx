2d9bdb810051c0c50aa9deb6be531ce3e6d68278
Nicolas Rosal <nicolasrosal98@gmail.com>
2021-10-21 12:57:42 +0100

    Add link to main content in footer

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -2,6 +2,7 @@ function PageFooter(): JSX.Element {
   return (
     <footer>
       <p>SITE CONTENTS NOT-REALLY-COPYRIGHT-BUT-WHATEVER, 2003, NICHARD RG</p>
+      <a href="./components/MainContent">Back to Main Content</a>
     </footer>
   );
 }
