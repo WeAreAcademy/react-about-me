c3fe5faa24d1938d43dae6dd6985040519ce5e75
grace k <grace.kuperman5472@gmail.com>
2022-11-01 14:50:02 +0000

    text removed, woops

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -2,7 +2,6 @@ function PageFooter(): JSX.Element {
   return (
     <footer>
       <p>SITE CONTENTS NOT-REALLY-COPYRIGHT-BUT-WHATEVER, 2003, NICHARD RG</p>
-      <p> change submitted by grace, hello beautiful world!</p>
     </footer>
   );
 }
