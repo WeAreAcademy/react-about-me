4ae6358d623b2fcf2c77fc12abd4a408f5a4b068
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2022-11-01 13:46:43 +0000

    changing the welcome message

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -2,7 +2,7 @@ function PageHeader(): JSX.Element {
   return (
     <header>
       <h1>This is now Maghfoor's website😈😈</h1>
-      <p>Welcome to Nichard's Ye Olde Web Page!</p>
+      <p>Welcome to MY WEBSITE. I CONTROL EVERYTHINGGGGGGG HERE</p>
       <p>
         This shows a random scatter of my thoughts, last updated on March 4,
         2003.
