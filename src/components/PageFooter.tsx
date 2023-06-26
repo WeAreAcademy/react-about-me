03728484841b9948de158113e047dc9fc7a0bd2a
TomOwen93 <mr.tsowen@gmail.com>
2023-06-26 16:13:15 +0100

    add additional text to PageFooter

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -1,7 +1,10 @@
 function PageFooter(): JSX.Element {
   return (
     <footer>
-      <p>SITE CONTENTS NOT-REALLY-COPYRIGHT-BUT-WHATEVER, 2003, NICHARD RG</p>
+      <p>
+        TESTING SITE CONTENTS NOT-REALLY-COPYRIGHT-BUT-WHATEVER, 2003, NICHARD
+        RG
+      </p>
     </footer>
   );
 }
