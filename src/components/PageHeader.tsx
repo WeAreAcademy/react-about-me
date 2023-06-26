5b2b57e507bb0848fa0dd4a94956973a65e1d8d9
oskartyrala <oskartyrala@protonmail.com>
2023-06-26 22:24:45 +0200

    Replace welcome with hiya

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -2,10 +2,10 @@ function PageHeader(): JSX.Element {
   return (
     <header>
       <h1>Ye Olde Web Page</h1>
-      <p>Welcome to Nichard's Ye Olde Web Page!</p>
+      <p>Hiya to Nichard's Ye Olde Web Page!</p>
       <p>
         This shows a random scatter of my thoughts, last updated on March 4,
-        2003.
+        2003. 
       </p>
     </header>
   );
