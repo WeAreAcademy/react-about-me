6776d0154257ef29050131d14cfeb2e94303acc0
Julieta-Sanguedolce <julieta.sanguedolce@gmail.com>
2023-06-27 20:19:42 +0100

    added julieta message

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -1,6 +1,7 @@
 function PageFooter(): JSX.Element {
   return (
     <footer>
+      <p>JULIETA ESTA HACIENDO PRUEBAS</p>
       <p>SITE CONTENTS NOT-REALLY-COPYRIGHT-BUT-WHATEVER, 2003, NICHARD RG</p>
     </footer>
   );
